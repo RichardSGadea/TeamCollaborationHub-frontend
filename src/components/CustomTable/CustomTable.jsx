fa0ea5065b1655991bf4804ded05671d9aadac94
Richard <richardsgadea@gmail.com>
2024-05-18 02:02:09 +0200

    feat: Added group info in group view

--- src/components/CustomTable/CustomTable.jsx
+++ src/components/CustomTable/CustomTable.jsx
@@ -2,11 +2,25 @@ import { useSelector } from "react-redux";
 import Table from 'react-bootstrap/Table';
 import { getUserData } from "../../app/Slices/userSlice";
 import "./CustomTable.css"
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function CustomTable({ dataProp, columnProp }) {
+function CustomTable({ dataProp, columnProp, numberGroup }) {
 
     const user = useSelector(getUserData)
+    const [locationUrl, setLocationUrl] = useState("")
+
+    const location = useLocation()
+
+    useEffect(() => {
+        const fetchUrl = () => {
+            setLocationUrl(location.pathname)
+        }
+        fetchUrl()
+        
+    }, [locationUrl])
+
+    
 
     return (
         <Table>
@@ -20,18 +34,32 @@ function CustomTable({ dataProp, columnProp }) {
                 </tr>
             </thead>
             <tbody>
-                {dataProp.map((item) => {
-                    return (
-                        <tr key={item.id}>
-                            <td><Link to={`/group/${item.id}`}>{item.name}</Link></td>
-                            {user.decoded.userRole === "teacher" &&
-                                <td>
-                                    <button className="iconActionsTeacher-design"><img src="../../trash.png" width="20px" height="20px" alt="" /></button>
-                                    <button className="iconActionsTeacher-design"><img src="../../addStudentIcon.png" width="20px" height="20px" alt="" /></button>
-                                </td>}
-                        </tr>
-                    )
-                })}
+                {
+                    locationUrl === "/" ? ((
+                        dataProp.map((item) => {
+                            return (<tr key={item.id}>
+                                <td><Link to={`/group/${item.id}`}>{item.name}</Link></td>
+                                {user.decoded.userRole === "teacher" &&
+                                    <td>
+                                        <button className="iconActionsTeacher-design"><img src="../../trash.png" width="20px" height="20px" alt="" /></button>
+                                        <button className="iconActionsTeacher-design"><img src="../../addStudentIcon.png" width="20px" height="20px" alt="" /></button>
+                                    </td>}
+                            </tr>)
+                        }))) : locationUrl === `/group/${numberGroup}` ?
+                        (
+                            dataProp.map((item) => {
+                                return (<tr key={item.id}>
+                                    {item.roleId !== 2 && <>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.email}</td>
+                                    </>
+                                    }
+
+                                </tr>)
+                            })
+                        ) : (<></>)
+                }
             </tbody>
         </Table>
     );
--- src/pages/Group/Group.jsx
+++ src/pages/Group/Group.jsx
@@ -1,14 +1,17 @@
 import { useParams } from "react-router-dom"
-import "./Group.css"
 import { useEffect, useState } from "react";
 import { bringGroupById } from "../../services/apiCalls";
 import { useSelector } from "react-redux";
 import { getUserData } from "../../app/Slices/userSlice";
+import "./Group.css"
+import CustomTable from "../../components/CustomTable/CustomTable";
 
 export const Group = () => {
 
-    const {groupId} = useParams();
+    const { groupId } = useParams();
     const [groupData, setGroupData] = useState(null);
+
+    const columnNames = [{id:1,name:"FirstName"},{id:2,name:"LastName"},{id:3,name:"Email"}]
 
     const user = useSelector(getUserData)
     const token = user.token
@@ -20,9 +23,9 @@ export const Group = () => {
     const fetchGroupData = async (id) => {
         //Retrieve group data
         try {
-            const data = await bringGroupById(token,id)
+            const data = await bringGroupById(token, id)
             setGroupData(data);
-            
+
         } catch (error) {
             console.log(error);
         }
@@ -32,12 +35,29 @@ export const Group = () => {
         return <div>Loading data...</div>;
     }
 
-    return(
-        <>
-            <h1>Group</h1>
-            <div>
-                <h2>{groupData.name}</h2>
+    return (
+        <div className="container-fluid groupBox-style">
+            <div className="row">
+                <div className="col-12 col-lg-4">
+                    <h1 className="viewTitle-design">Group</h1>
+                    <div>
+                        <h2 className="groupName-design">{groupData.name}</h2>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-7">
+                    <h2 className="groupName-design mt-2">Students</h2>
+                    <CustomTable 
+                        dataProp={groupData.users}
+                        columnProp={columnNames}
+                        numberGroup={groupId}
+                    />
+                    <h2 className="groupName-design mt-5">Tasks</h2>
+                    <p className="statesTasks-design">Tasks To Do: <span className="text-danger"></span></p>
+                    <p className="statesTasks-design">Tasks In Progress: <span className="text-info"></span></p>
+                    <p className="statesTasks-design">Completed Tasks: <span className="text-success"></span></p>
+                </div>
             </div>
-        </>
+
+        </div>
     )
 }
